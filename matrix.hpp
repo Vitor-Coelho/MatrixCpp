5fc1b889e779e928eb5f7d1d6a9170914713436c
Vítor Lasserré Nunes Coêlho <vitorlnc@hotmail.com>
2022-06-24 20:07:19 -0300

    add template to provide mutiple matrix types

--- matrix.hpp
+++ matrix.hpp
@@ -16,50 +16,49 @@
 // • colocar mais parâmetros no construtor para alterar a forma do random (média, desvio padrão, uniforme vs normal)
 // • colocar o operador () para pegar valores e [] para pegar submatrizes -> será importante para conv NN
 //   ex.: A[2, 3, 0, 2] pegaria as linhas 2 e 3 com as colunas 0 a 2 -> Matriz 2x3
-// • modificar os métodos para não alterar in-place (pois basta utilizar o operador =)
-//   ex.: A = A*B ou A = A.multiply(B)
-// • adicionar o template <typename T> para poder criar as matrizes de float, double, int, etc
-// • organizar os repositórios para ficar apenas os arquivos importantes (colocar .gitignore e >> git rm)
 
 
-class Matrix{
+
+/* Class definitions */
+template <typename T> class Matrix{
     private:
         size_t rows, cols;
-        std::vector<float> values;
+        std::vector<T> values;
 
     public:
         Matrix();
-        Matrix(size_t numRows, size_t numCols, std::string fill="zeros");
-        Matrix(std::vector<float> vec, size_t numRows, size_t numCols);
+        Matrix(size_t numRows, size_t numCols, const char * fill="zeros");
+        template<typename dist_t> Matrix(size_t numRows, size_t numCols, dist_t dist);
+        Matrix(std::vector<T> vec, size_t numRows, size_t numCols);
         ~Matrix();
 
         void print();
 
-        void set(float value, size_t row, size_t col);
-        void set(std::vector<float> vec, size_t idx, bool row=true);
-        void set(Matrix matrix);
+        void set(T value, size_t row, size_t col);
+        void set(std::vector<T> vec, size_t idx, bool row=true);
+        void set(Matrix<T> matrix);
 
         bool equalTo(Matrix matrix);
 
         size_t numRows();
         size_t numCols();
-        std::vector<float>* getValues();
+        std::vector<T>* getValues();
 
         void setRows(size_t newRows);
         void setCols(size_t newCols);
 
-        Matrix add(float value);
+        Matrix add(T value);
         Matrix add(Matrix matrix);
-        Matrix subtract(float value);
+        Matrix subtract(T value);
         Matrix subtract(Matrix matrix);
-        Matrix multiply(float value);
+        Matrix multiply(T value);
         Matrix multiply(Matrix matrix);
-        Matrix divide(float value);
-        float dotProduct(Matrix matrix);
-        float sum();
+        Matrix divide(T value);
+        T dotProduct(Matrix matrix);
+        T sum();
         Matrix transpose();
 
-        Matrix applyFunction(float function(float));
+        Matrix applyFunction(T function(T));
         Matrix insert(Matrix toAppend, size_t idx, bool row=true);
         Matrix append(Matrix toAppend, bool row=true);
         Matrix del(size_t startIdx, size_t endIdx, bool row=true);
@@ -67,30 +66,477 @@ class Matrix{
         //Matrix crossProduct(Matrix matrix);   //TODO
 
         // Overloaded operators
-        float operator()(size_t row, size_t col);
+        T operator()(size_t row, size_t col);
         Matrix operator()(size_t idx, bool row=true);
 
         void operator=(Matrix matrix);
         bool operator==(Matrix matrix);
 
         Matrix operator+(Matrix matrix);
-        Matrix operator+(float value);
+        Matrix operator+(T value);
         Matrix operator-(Matrix matrix);
-        Matrix operator-(float value);
+        Matrix operator-(T value);
         Matrix operator*(Matrix matrix);
-        Matrix operator*(float value);
-        Matrix operator/(float value);
+        Matrix operator*(T value);
+        Matrix operator/(T value);
 
         void operator++(int);
         void operator--(int);
 
         void operator+=(Matrix matrix);
-        void operator+=(float value);
+        void operator+=(T value);
         void operator-=(Matrix matrix);
-        void operator-=(float value);
-        void operator*=(Matrix matrix);
-        void operator*=(float value);
-        void operator/=(float value);
+        void operator-=(T value);
+        void operator*=(Matrix<T> matrix);
+        void operator*=(T value);
+        void operator/=(T value);
 };
+
+
+
+/* Random utilities */
+std::mt19937 mt(time(NULL));
+
+
+/* Constructors and destructor */
+template <typename T>
+Matrix<T>::Matrix(){
+    this->rows = 1;
+    this->cols = 1;
+    this->values.resize(1, 0);    
+}
+
+template <typename T>
+Matrix<T>::Matrix(size_t numRows, size_t numCols, const char * fill){
+    this->rows = numRows;
+    this->cols = numCols;
+
+    if(fill == (const char *) "zeros"){
+        this->values.resize(numCols*numRows, 0);
+    }else if(fill == (const char *) "ones"){
+        this->values.resize(numCols*numRows, 1);
+    }
+}
+
+template <typename T>
+template <typename dist_t>
+Matrix<T>::Matrix(size_t numRows, size_t numCols, dist_t dist){
+    this->rows = numRows;
+    this->cols = numCols;
+
+    this->values.resize(numCols*numRows, 0);
+    for(size_t i = 0; i < this->rows; i++){
+        for(size_t j = 0; j < this->cols; j++){
+            this->set(dist(mt), i, j);
+        }
+    }
+}
+
+template <typename T>
+Matrix<T>::Matrix(std::vector<T> vec, size_t numRows, size_t numCols){
+    this->rows = numRows;
+    this->cols = numCols;
+    this->values.assign(vec.begin(), vec.begin() + vec.size());
+}
+
+template <typename T>
+Matrix<T>::~Matrix(){}
+
+
+
+/* Getters and Setters */
+template <typename T>
+size_t Matrix<T>::numRows(){
+    return this->rows;
+}
+
+template <typename T>
+size_t Matrix<T>::numCols(){
+    return this->cols;
+}
+
+template <typename T>
+std::vector<T>* Matrix<T>::getValues(){
+    return &values;
+}
+
+template <typename T>
+void Matrix<T>::setRows(size_t newRows){
+    rows = newRows;
+}
+
+template <typename T>
+void Matrix<T>::setCols(size_t newCols){
+    cols = newCols;
+}
+
+template <typename T>
+void Matrix<T>::set(T value, size_t row, size_t col){
+    this->values.at(col + this->cols*row) = value;
+}
+
+template <typename T>
+void Matrix<T>::set(std::vector<T> vec, size_t idx, bool row){
+    if(row){
+        for(size_t j = 0; j < this->cols; j++){
+            this->set(vec.at(j), idx, j);
+        }
+    }else{
+        for(size_t i = 0; i < this->rows; i++){
+            this->set(vec.at(i), i, idx);
+        }
+    }  
+}
+
+template <typename T>
+void Matrix<T>::set(Matrix matrix){
+    this->rows = matrix.rows;
+    this->cols = matrix.cols;
+    this->values.assign(matrix.values.begin(), matrix.values.begin() + matrix.values.size());
+}
+
+
+
+/* Print matrix */
+template <typename T>
+void Matrix<T>::print(){
+    std::cout << std::setprecision(5);
+    std::cout << std::fixed;
+    std::cout << "\n";
+    for(size_t i = 0; i < this->rows; i++){
+        std::cout << "|";
+        for(size_t j = 0; j < this->cols; j++){
+            std::cout << std::setw(10) << (*this)(i,j) << " ";
+        }
+        std::cout << "|\n";
+    }
+    std::cout << "\n";
+}
+
+
+
+/* Equality comparison */
+template <typename T>
+bool Matrix<T>::equalTo(Matrix matrix){
+    if(this->rows == matrix.rows && this->cols == matrix.cols){
+        for(size_t i = 0; i < matrix.rows; i++){
+            for(size_t j = 0; j < matrix.cols; j++){
+                if((*this)(i,j) != matrix(i,j))
+                    return false;
+            }
+        }
+        return true;
+    }else{
+        return false;
+    }
+}
+
+
+
+/* Matrix operations */
+template <typename T>
+Matrix<T> Matrix<T>::add(T value){
+    Matrix result(this->rows, this->cols);
+
+    for(size_t i = 0; i < this->rows; i++){
+        for(size_t j = 0; j < this->cols; j++){
+            result.set((*this)(i,j) + value, i, j);
+        }
+    }
+
+    return result;
+}
+
+template <typename T>
+Matrix<T> Matrix<T>::add(Matrix matrix){
+    if(this->rows != matrix.rows || this->cols != matrix.cols)
+        throw std::invalid_argument("Matrices must have the same format");
+
+    Matrix result(this->rows, this->cols);
+
+    for(size_t i = 0; i < this->rows; i++){
+        for(size_t j = 0; j < this->cols; j++){
+            result.set((*this)(i,j) + matrix(i,j), i, j);
+        }
+    }
+    return result;
+}
+
+template <typename T>
+Matrix<T> Matrix<T>::subtract(T value){
+    Matrix result(this->rows, this->cols, "zeros");
+
+    for(size_t i = 0; i < this->rows; i++){
+        for(size_t j = 0; j < this->cols; j++){
+            result.set((*this)(i,j) - value, i, j);
+        }
+    }
+    return result;
+}
+
+template <typename T>
+Matrix<T> Matrix<T>::subtract(Matrix matrix){
+    if(this->rows != matrix.rows || this->cols != matrix.cols)
+        throw std::invalid_argument("Matrices must have the same format");
+
+    Matrix result(this->rows, this->cols, "zeros");
+
+    for(size_t i = 0; i < this->rows; i++){
+        for(size_t j = 0; j < this->cols; j++){
+            result.set((*this)(i,j) - matrix(i,j), i, j);
+        }
+    }
+    return result;
+}
+
+template <typename T>
+Matrix<T> Matrix<T>::multiply(T value){
+    Matrix result(this->rows, this->cols, "zeros");
+
+    for(size_t i = 0; i < this->rows; i++){
+        for(size_t j = 0; j < this->cols; j++){
+            result.set((*this)(i, j)*value, i, j);
+        }
+    }
+
+    return result;
+}
+
+template <typename T>
+Matrix<T> Matrix<T>::multiply(Matrix matrix){
+    if(this->cols != matrix.rows)
+        throw std::invalid_argument("First matrix column size must match second matrix row size");
+
+    Matrix result(this->rows, matrix.cols, "zeros");
+
+    for(size_t i = 0; i < this->rows; i++){
+        for(size_t j = 0; j < matrix.cols; j++){
+            float dotProd = (*this)(i,true).dotProduct(matrix(j, false).transpose());
+            result.set(dotProd, i, j);
+        }
+    }
+
+    return result;
+}
+
+template <typename T>
+Matrix<T> Matrix<T>::divide(T value){
+    Matrix result(this->rows, this->cols, "zeros");
+
+    for(size_t i = 0; i < this->rows; i++){
+        for(size_t j = 0; j < this->cols; j++){
+            result.set((*this)(i, j)/value, i, j);
+        }
+    }
+    return result;
+}
+
+template <typename T>
+T Matrix<T>::dotProduct(Matrix matrix){
+    if(this->rows != matrix.rows || this->cols != matrix.cols)
+        throw std::invalid_argument("Matrices must have the same format");
+
+    float dotProd = 0;
+    for(size_t i = 0; i < matrix.rows; i++){
+        for(size_t j = 0; j < matrix.cols; j++){
+            dotProd += (*this)(i,j) * matrix(i,j);
+        }
+    }
+
+    return dotProd;
+}
+
+template <typename T>
+T Matrix<T>::sum(){
+    float sum = 0;
+    for(size_t i = 0; i < rows; i++){
+        for(size_t j = 0; j < cols; j++){
+            sum += (*this)(i,j);
+        }
+    }
+    return sum;
+}
+
+template <typename T>
+Matrix<T> Matrix<T>::transpose(){
+    Matrix result(this->cols, this->rows, "zeros");
+
+    for(size_t i = 0; i < this->rows; i++){
+        for(size_t j = 0; j < this->cols; j++){
+            result.set((*this)(i,j), j, i);
+        }
+    }
+
+    return result;
+}
+
+template <typename T>
+Matrix<T> Matrix<T>::applyFunction(T function(T)){
+    Matrix result(this->rows, this->cols, "zeros");
+
+    for(size_t i = 0; i < this->rows; i++){
+        for(size_t j = 0; j < this->cols; j++){
+            result.set(function((*this)(i,j)), i, j);
+        }
+    }
+    return result;
+}
+
+
+
+/* Inserting, appending and deleting */
+template <typename T>
+Matrix<T> Matrix<T>::insert(Matrix toAppend, size_t idx, bool row){
+    Matrix result(*(this->getValues()), this->rows, this->cols);
+
+    if(row){
+        if(cols != toAppend.cols)
+            throw std::invalid_argument("Matrices must have the same number of columns");
+
+        result.getValues()->insert(result.getValues()->begin()+result.numCols()*idx, 
+                                  (*toAppend.getValues()).begin(),
+                                  (*toAppend.getValues()).end());
+
+        result.rows = result.rows + toAppend.numRows();
+
+    }else{
+        if(rows != toAppend.rows)
+            throw std::invalid_argument("Matrices must have the same number of rows");
+
+        size_t end = result.getValues()->size();
+        size_t numInserted = 0;
+        for(size_t i = 0; i < end; i += cols){
+            result.getValues()->insert(result.getValues()->begin() + i + numInserted + idx, (*toAppend.getValues()).at(i/cols));
+            numInserted++;
+        }
+
+        result.cols = cols + toAppend.numCols();
+    }
+    
+    return result;
+}
+
+template <typename T>
+Matrix<T> Matrix<T>::append(Matrix toAppend, bool row){
+    Matrix result(*(this->getValues()), this->rows, this->cols);
+
+    if(row)
+        result = result.insert(toAppend, rows, true);
+    else
+        result = result.insert(toAppend, cols, false);
+    
+    return result;
+}
+
+template <typename T>
+Matrix<T> Matrix<T>::del(size_t startIdx, size_t endIdx, bool row){
+    // Start idx and end idx are inclusive [startIdx, endIdx]
+    Matrix result(*(this->getValues()), this->rows, this->cols);
+
+    if(row){
+        if(startIdx < 0 || startIdx >= result.rows || endIdx < 0 || endIdx >= result.rows || endIdx < startIdx)
+            throw std::invalid_argument("Indexes out of range");
+
+        result.values.erase(result.values.begin() + startIdx*result.cols, result.values.begin() + (endIdx+1)*result.cols);
+
+        result.rows = rows - (endIdx - startIdx + 1);
+
+    }else{
+        if(startIdx < 0 || startIdx >= this->cols || endIdx < 0 || endIdx >= this->cols || endIdx < startIdx)
+            throw std::invalid_argument("Indexes out of range");
+
+        size_t end = result.values.size();
+        size_t numDeleted = 0;
+        for(size_t i = 0; i < end; i += cols){
+            for(size_t col = startIdx; col <= endIdx; col++){
+                result.values.erase(result.values.begin() + i + col - numDeleted);
+                numDeleted++;
+            }
+        }
+
+        result.cols = cols - (endIdx - startIdx + 1);
+    }
+    
+    return result;
+}
+
+
+
+/* Overloaded operators */
+template <typename T>
+T Matrix<T>::operator()(size_t row, size_t col){
+    return this->values.at(col + this->cols*row);
+}
+
+template <typename T>
+Matrix<T> Matrix<T>::operator()(size_t idx, bool row){
+    if(row){
+        Matrix result(1, this->cols, "zeros");
+        for(size_t j = 0; j < this->cols; j++){
+            result.set((*this)(idx, j), 0, j);
+        }
+        return result;
+    }else{
+        Matrix result(this->rows, 1, "zeros");
+        for(size_t i = 0; i < this->rows; i++){
+            result.set((*this)(i, idx), i, 0);
+        }
+        return result;
+    }
+}
+
+template <typename T>
+void Matrix<T>::operator=(Matrix matrix){this->set(matrix);}
+
+template <typename T>
+bool Matrix<T>::operator==(Matrix matrix){return this->equalTo(matrix);}
+
+template <typename T>
+Matrix<T> Matrix<T>::operator+(Matrix matrix){return this->add(matrix);}
+
+template <typename T>
+Matrix<T> Matrix<T>::operator+(T value)  {return this->add(value);}
+
+template <typename T>
+Matrix<T> Matrix<T>::operator-(Matrix matrix){return this->subtract(matrix);}
+
+template <typename T>
+Matrix<T> Matrix<T>::operator-(T value)  {return this->subtract(value);}
+
+template <typename T>
+Matrix<T> Matrix<T>::operator*(Matrix matrix){return this->multiply(matrix);}
+
+template <typename T>
+Matrix<T> Matrix<T>::operator*(T value)  {return this->multiply(value);}
+
+template <typename T>
+Matrix<T> Matrix<T>::operator/(T value)  {return this->divide(value);}
+
+template <typename T>
+void Matrix<T>::operator++(int){(*this) = this->add(1);}
+
+template <typename T>
+void Matrix<T>::operator--(int){(*this) = this->subtract(1);}
+
+template <typename T>
+void Matrix<T>::operator+=(Matrix matrix){(*this) = this->add(matrix);}
+
+template <typename T>
+void Matrix<T>::operator+=(T value)  {(*this) = this->add(value);}
+
+template <typename T>
+void Matrix<T>::operator-=(Matrix matrix){(*this) = this->subtract(matrix);}
+
+template <typename T>
+void Matrix<T>::operator-=(T value)  {(*this) = this->subtract(value);}
+
+template <typename T>
+void Matrix<T>::operator*=(Matrix<T> matrix){(*this) = this->multiply(matrix);}
+
+template <typename T>
+void Matrix<T>::operator*=(T value)  {(*this) = this->multiply(value);}
+
+template <typename T>
+void Matrix<T>::operator/=(T value)  {(*this) = this->divide(value);}
 
 #endif
--- test.cpp
+++ test.cpp
@@ -5,16 +5,19 @@ using namespace std;
 
 
 int main(){
-    Matrix matrix(3, 3, (string) "random");
-    Matrix matrixColumn(3, 1, (string) "random");
-    Matrix matrixRow(1, 3, (string) "random");
-    Matrix matrixResult;
+    std::normal_distribution<float> dist(-1, 1);
+
+    Matrix<float> matrix(3, 3, dist);
+    Matrix<float> matrixColumn(3, 1, dist);
+    Matrix<float> matrixRow(1, 3, dist);
+    Matrix<float> matrixResult;
 
 
     cout << "\n*** Testing matrices ***\n";
     matrix.print();
     matrixColumn.print();
     matrixRow.print();
+    matrixResult.print();
 
     cout << "\n*** Testing append, insert e del ***\n";
     cout << "Append row\n";
@@ -60,31 +63,31 @@ int main(){
     cout << (matrix == matrixResult) << endl;
 
     cout << "\n*** Testing operations ***\n";
-    matrixResult = Matrix(matrix.numRows(), matrix.numCols(), "ones");
+    matrixResult = Matrix<float>(matrix.numRows(), matrix.numCols(), dist);
     matrixResult.print();
     matrixResult = matrixResult + matrixResult;
     matrixResult.print();
     matrixResult = matrixResult * 2;
     matrixResult.print();
-    cout << "Dot product: " << matrixResult.dotProduct(Matrix(matrixResult.numRows(), matrixResult.numCols(), "ones")) << endl;
+    cout << "Dot product: " << matrixResult.dotProduct(Matrix<float>(matrixResult.numRows(), matrixResult.numCols(), "ones")) << endl;
     cout << "Sum: " << matrixResult.sum() << endl;
-    matrix = Matrix(matrix.numRows(), matrix.numCols(), "ones");
-    matrixResult = Matrix(matrix.numRows(), matrix.numCols(), "ones");
+    matrix = Matrix<float>(matrix.numRows(), matrix.numCols(), "ones");
+    matrixResult = Matrix<float>(matrix.numRows(), matrix.numCols(), "ones");
     matrixResult = matrix * matrixResult;
     matrixResult.print();
 
     cout << "\n*** Testing applying function and transposing ***\n";
-    matrixResult = Matrix(matrix.numRows(), matrix.numCols(), "ones") * 2;
+    matrixResult = Matrix<float>(matrix.numRows(), matrix.numCols(), "ones") * 2;
     matrixResult = matrixResult.applyFunction([](float x){return 1/x;});
     matrixResult.print();
 
-    matrixResult = Matrix(matrix.numRows(), matrix.numCols(), "random");
+    matrixResult = Matrix<float>(matrix.numRows(), matrix.numCols(), dist);
     matrixResult.print();
     matrixResult = matrixResult.transpose();
     matrixResult.print();
 
     cout << "\n*** Testing inplace operations ***\n";
-    matrixResult = Matrix(matrix.numRows(), matrix.numCols(), "ones");
+    matrixResult = Matrix<float>(matrix.numRows(), matrix.numCols(), "ones");
     matrixResult.print();
     matrixResult += matrixResult;
     matrixResult.print();
@@ -92,8 +95,8 @@ int main(){
     matrixResult.print();
     matrixResult -= 2;
     matrixResult.print();
-    matrix = Matrix(matrix.numRows(), matrix.numCols(), "ones");
-    matrixResult = Matrix(matrix.numRows(), matrix.numCols(), "ones");
+    matrix = Matrix<float>(matrix.numRows(), matrix.numCols(), "ones");
+    matrixResult = Matrix<float>(matrix.numRows(), matrix.numCols(), "ones");
     matrixResult *= matrix;
     matrixResult.print();
 
